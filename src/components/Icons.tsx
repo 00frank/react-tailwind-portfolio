c69e88d4c2bb9c11486040135e82967c176a89c8
Frank <00frankgarcia@gmail.com>
2024-04-09 15:31:56 -0300

    adding new tech icons

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,9 +1,21 @@
 import { IoLogoLinkedin } from "react-icons/io";
-import { BsSpotify, BsPlayFill, BsPauseFill } from "react-icons/bs";
+import { BsSpotify, BsPlayFill, BsPauseFill, BsStack } from "react-icons/bs";
+import { FaLink, FaGolang } from "react-icons/fa6";
+
+import { FaReact, FaNodeJs, FaAws } from "react-icons/fa";
+import { SiNestjs, SiTypescript } from "react-icons/si";
 
 export {
   IoLogoLinkedin as LinkedinIcon,
   BsSpotify as SpotifyIcon,
   BsPlayFill as PlayMediaIcon,
   BsPauseFill as PauseMediaIcon,
+  FaLink as LinkIcon,
+  BsStack as StackIcon,
+  FaGolang as GolangIcon,
+  FaReact as ReactIcon,
+  FaNodeJs as NodeJsIcon,
+  FaAws as AwsIcon,
+  SiNestjs as NestJsIcon,
+  SiTypescript as TypescriptIcon,
 }
