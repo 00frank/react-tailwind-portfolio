2ebfca97cc5771dc8a9b967a43dec7414d34f0ed
Frank <00frankgarcia@gmail.com>
2024-04-09 15:32:30 -0300

    adding redirection link to card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,11 +3,20 @@ import { PropsWithChildren } from "react";
 interface CardProps extends PropsWithChildren {
   border?: boolean;
   className?: string;
+  to?: string | null;
 }
 
-function Card({ border = false, className = "", children }: CardProps) {
+function Card({ border = false, className = "", to = null, children }: CardProps) {
+  if (!!to)
+    return (
+      <a
+        className={`w-full max-w-[652px] md:w-96 block ${border ? "border border-gray-600 rounded-xl " : ""}${className ? " " + className : ""}`}
+        href={to}>
+        {children}
+      </a>
+    )
   return (
-    <div className={`max-w-96 ${border ? "border border-gray-600 rounded-xl " : ""}${className ? " " + className : ""}`}>
+    <div className={`w-full max-w-[652px] md:w-96 ${border ? "border border-gray-600 rounded-xl " : ""}${className ? " " + className : ""}`}>
       {children}
     </div>
   )
