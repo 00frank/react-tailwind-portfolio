04f032ed3184315f80e3d7f739c02beca79a9057
00frank <00frankgarcia@gmail>
2024-05-27 20:37:27 -0300

    fix in image gallery

--- src/cards/Gallery.tsx
+++ src/cards/Gallery.tsx
@@ -1,64 +1,66 @@
-import Card from "@/components/Card";
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
+import Carousel, { ResponsiveType } from "react-multi-carousel";
 
+import Card from "@/components/Card";
+import useWindowDimensions from "@/hooks/useWindowDimensions";
+
+// shows better with 2:3 images
 const images = [
+  "https://images.unsplash.com/photo-1712673363052-0d603997af16?q=80&w=1996&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1542976735-f64ecb935c2d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1713213915575-0c88efc9149e?q=80&w=2016&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  "https://images.unsplash.com/photo-1712673363052-0d603997af16?q=80&w=1996&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1706928600672-dc5263cd759b?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   "https://images.unsplash.com/photo-1713051496669-e8ca782ace1b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
 ]
 
-const responsive = {
+const responsive: ResponsiveType = {
   desktop: {
-    breakpoint: { max: 3000, min: 1024 },
+    breakpoint: { max: 3000, min: 0 },
     items: 1,
-    slidesToSlide: 3 // optional, default to 1.
   },
   tablet: {
     breakpoint: { max: 1024, min: 464 },
-    items: 2,
-    slidesToSlide: 2 // optional, default to 1.
+    items: 1,
   },
   mobile: {
     breakpoint: { max: 464, min: 0 },
     items: 1,
-    slidesToSlide: 1 // optional, default to 1.
   }
 };
 
 function Gallery() {
-  const styles = {
-    display: 'block',
+  const { width } = useWindowDimensions()
+  const containerSize = width >= 768 ? "100%" : width - (34)
+
+  const styles: React.CSSProperties = {
+    objectFit: "contain",
+    display: "block",
     height: '100%',
+    width: "auto",
     margin: 'auto',
-    width: '100%'
   }
 
   return (
-    <Card>
-      <Carousel
-        swipeable={false}
-        draggable={false}
-        showDots={true}
-        responsive={responsive}
-        infinite={true}
-        // autoPlay={this.props.deviceType !== "mobile" ? true : false}
-        autoPlaySpeed={1000}
-        keyBoardControl={true}
-        customTransition="all .5"
-        transitionDuration={500}
-        containerClass="carousel-container"
-        removeArrowOnDeviceType={["tablet", "mobile"]}
-        // deviceType={this.props.deviceType}
-        dotListClass="custom-dot-list-style"
-        itemClass="carousel-item-padding-40-px"
-      >
-        {images.map((el, i) => (
-          <img src={el} key={i} style={styles} />
-        ))}
-      </Carousel>
+    <Card border className="overflow-hidden">
+      <div className="flex justify-center">
+        <div style={{ width: containerSize }}>
+          <Carousel
+            swipeable={true}
+            draggable={false}
+            showDots={true}
+            responsive={responsive}
+            infinite={true}
+            keyBoardControl={true}
+            customTransition="all .5"
+            containerClass="carousel-container"
+            removeArrowOnDeviceType={[]}
+            dotListClass="!bottom-5 custom-dot-list-style"
+          >
+            {images.map((el, i) => (
+              <img src={el} alt={`image-${i}`} key={i} style={styles} />
+            ))}
+          </Carousel>
+        </div>
+      </div>
     </Card>
   )
 }
