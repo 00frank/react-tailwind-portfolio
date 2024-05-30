2b169d867f143610822aacfb3fd928203aec01d7
00frank <00frankgarcia@gmail>
2024-05-30 20:37:15 -0300

    adding playable music card

--- src/cards/Spotify.tsx
+++ src/cards/Spotify.tsx
@@ -1,39 +1,49 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 import Card from "@/components/Card"
 import { PlayMediaIcon, PauseMediaIcon, SpotifyIcon } from "@/components/Icons"
 
 import spotifyCover from "@/assets/spotify-cover.png"
+import song from "@/assets/Emil Rottmayer - Descend.mp3"
+
+const spotifySong = {
+  url: "https://open.spotify.com/intl-es/track/5qibxzFe45UtLxVA4W4aHg?si=44c72f8b0fd64295",
+  mp3: song,
+  cover: spotifyCover
+};
 
 function Spotify() {
   const [playing, setPlaying] = useState(false)
+  const audioRef = useRef(new Audio(spotifySong.mp3));
+
+  useEffect(() => {
+    audioRef.current.volume = 0.3
+    if (playing)
+      audioRef.current.play()
+
+    if (!playing)
+      audioRef.current.pause()
+  }, [playing])
+
   return (
     <Card className="bg-gradient-to-tr rounded-xl p-4 from-lime-950/50 from-30% to-[#1FDD63]/60">
-      <div className="flex absolute blur">
-        <div
-          className="relative w-12 h-12 cursor-pointer select-none"
-          onClick={() => setPlaying(!playing)}>
-          <img className={`${playing ? "animate-spin blur-sm" : ""} transition-all w-11 h-11 rounded-full`} src={spotifyCover} alt="spotifyCover" />
-        </div>
-      </div>
-      {/* blur effect up - new changes from here */}
       <div className="flex">
         <div
           className="relative w-12 h-12 cursor-pointer select-none"
           onClick={() => setPlaying(!playing)}>
-          <img className={`${playing ? "animate-spin-slow shadow-slate-400 blur-sm " : ""}transition-all w-12 h-12 rounded-full`} src={spotifyCover} alt="spotifyCover" />
-          <div className="bg-white text-black absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center w-7 h-7 rounded-full">
+          <img className={`${playing ? "animate-spin-slow shadow-slate-400 " : ""}transition-all w-12 h-12 rounded-full`} src={spotifySong.cover} alt="spotifyCover" />
+          <div className="bg-white text-black absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center w-6 h-6 rounded-full">
             {!playing && <PlayMediaIcon />}
             {!!playing && <PauseMediaIcon />}
           </div>
         </div>
         <div className="pl-4 flex flex-col text-wrap break-words">
-          <p className="text-gray-200/90">Soamairi</p>
-          <p className="font-semibold">tokyo dream - lofi</p>
+          <p className="text-gray-200/90">Emil Rottmayer</p>
+          <p className="font-semibold">Descend</p>
         </div>
-        <div className="flex justify-end items-end flex-1">
+        <a href={spotifySong.url} target="__blank" className="flex justify-end items-end flex-1">
           <SpotifyIcon className="text-xl" />
-        </div>
+        </a>
       </div>
     </Card>
   )
