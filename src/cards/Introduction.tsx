8517f59c39ba24854d4fcc7f0dbb83a760dedf52
00frank <00frankgarcia@gmail>
2024-06-11 17:14:01 -0300

    adding image from introduction changeable

--- src/cards/Introduction.tsx
+++ src/cards/Introduction.tsx
@@ -1,7 +1,10 @@
-import Card from '../components/Card'
-import reactSvg from '../assets/react.svg'
-import Button from '../components/Button'
-import { LinkedinIcon } from '../components/Icons'
+import { useState } from 'react'
+import Card from '@/components/Card'
+import Button from '@/components/Button'
+import { LinkedinIcon } from '@/components/Icons'
+
+import reactSvg from '@/assets/react.svg'
+import profile from '@/assets/profile.png'
 
 const ARFlag = () => {
   return (
@@ -10,16 +13,31 @@ const ARFlag = () => {
 }
 
 const Introduction = () => {
+  const [imageShowing, setImageShowing] = useState(false);
   return (
     <Card border className="p-12">
-      <div className="mb-6 relative">
-        <img src={reactSvg} alt="reactSvg" />
-        <img src={reactSvg} className="blur-md absolute top-0" alt="reactSvg" />
+      <div className="mb-6 w-9 h-8 relative">
+        <img
+          hidden={imageShowing}
+          src={reactSvg}
+          alt="reactSvg" />
+        <img
+        onClick={() => setImageShowing(prev => !prev)}
+          hidden={imageShowing}
+          src={reactSvg}
+          className="blur-md absolute top-0"
+          alt="reactSvg" />
+        <img
+          hidden={!imageShowing}
+          onClick={() => setImageShowing(prev => !prev)}
+          className="rounded-full w-full"
+          src={profile}
+          alt="profile_image" />
       </div>
       <h1 className="text-4xl font-semibold mb-6">Frank <span hidden>García</span></h1>
       <p className="text-gray-200/70 mb-6 text-wrap">Cloud engineer based in Buenos Aires, AR <ARFlag />. Currently focused on my studies at{" "}
         <a className="text-white hover:underline" href="https://ucema.edu.ar/" target="_blank">UCEMA</a>{" "}
-        and <span className="text-white hover:underline">open for work</span> 
+        and <span className="text-white hover:underline">open for work</span>
         {/* and programming for{" "} */}
         {/* <a className="text-white hover:underline" href="#" target="_blank">example.co</a>. */}
       </p>
